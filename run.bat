901dd7b764fbc98a78f36c868e39c11686ab7723
dappsar <dapps.ar@gmail.com>
2023-01-30 13:40:19 -0300

    updated run scripts

--- run.bat
+++ run.bat
@@ -1,9 +1,3 @@
 start python cf.py -d3 -w1
 start python cf.py -d3 -w2
 start python cf.py -d3 -w3
-start python cf.py -d3 -w4
-start python cf.py -d3 -w5
-start python cf.py -d3 -w6
-start python cf.py -d3 -w7
-start python cf.py -d3 -w8
-start python cf.py -d3 -w9
